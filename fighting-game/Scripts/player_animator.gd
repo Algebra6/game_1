38194cfa60ad020791e2b61260523c3a25c9e50f
jiangyuhang0930 <74084213+jiangyuhang0930@users.noreply.github.com>
2026-07-19 12:02:22 -0700

    Add player animations
    added idle, walking, jumping animations

--- fighting-game/Scripts/player_animator.gd
+++ fighting-game/Scripts/player_animator.gd
@@ -11,15 +11,13 @@ func _process(delta):
 	elif player_controller.direction == -1:
 		sprite.flip_h = true
 		
-	animation_player.play("knight_idle")
 
-	# plays the movement animation
-	#if abs(player_controller.velocity.x) > 0.0:
-		#animation_player.play("move")
-	#else:
-		#animation_player.play("idle")
-	##plays the jump animation 
-	#if player_controller.velocity.y < 0.0:
-		#animation_player.play("jump")
-	#elif player_controller.velocity.y > 0.0:
-		#animation_player.play("fall")
+
+	##plays the movement animation
+	if abs(player_controller.velocity.x) > 0.0:
+		animation_player.play("knight_walk")
+	else:
+		animation_player.play("knight_idle")
+		
+	if player_controller.velocity.y != 0.0:
+		animation_player.play("knight_jump")	
